988926cca54b7321a2a3f601c31c8940059ddb45
adriannapinzariu <121136061+adriannapinzariu@users.noreply.github.com>
2023-06-25 17:08:14 -0500

    added photo

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import { Link as RouterLink } from 'react-router-dom';
-import { Box, VStack, Heading, Text, Link as ChakraLink, Button, useColorModeValue, Flex, Spacer, Avatar, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
+import { Box, VStack, Heading, Text, Link as ChakraLink, Button, useColorModeValue, Flex, Spacer, Avatar, Menu, MenuButton, MenuItem, MenuList, Image} from '@chakra-ui/react';
 import { ChevronDownIcon } from '@chakra-ui/icons';
 import { motion } from 'framer-motion';
 import { Html } from '@react-three/drei';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { Cylinder } from '@react-three/drei';
 import { MeshBasicMaterial } from 'three';
+import myImage from './me.png';
 
 
 const MotionBox = motion(Box);
@@ -53,37 +54,37 @@ const buttonVariants = {
 function Navbar() {
     return (
       <Flex p="2" bg="transparent" color="white" boxShadow="md" position="sticky" top={0} zIndex={3}>
-        <Heading size="md" ml="2">ASL Journey</Heading>
+        <Heading size="md" ml="2">My Portfolio</Heading>
         <Spacer />
-        <Menu>
-          <MenuButton bg="#2D3748"as={Button} borderWidth={2}
-            borderColor={useColorModeValue("#4b5178", "white")} rightIcon={<ChevronDownIcon />} color="black">
-            <Avatar size="sm" />
-          </MenuButton>
-          <MenuList bg="#000000"  >
-  <MenuItem bg="#000000" color="white" _hover={{ bg: "#333333" }}>Profile</MenuItem>
-  <MenuItem bg="#000000" color="white" _hover={{ bg: "#333333" }}>Settings</MenuItem>
-  <MenuItem bg="#000000" color="white" _hover={{ bg: "#333333" }}>Logout</MenuItem>
-</MenuList>
-        </Menu>
+        <RouterLink to="/contact">
+          <MotionButton
+            whileHover="hover"
+            whileTap="tap"
+            variants={buttonVariants}
+            colorScheme="purple"
+            size="lg"
+            color="white"
+          >
+            Contact Me
+          </MotionButton>
+        </RouterLink>
       </Flex>
     );
   }
-
-// Footer
-function Footer() {
-  return (
-    <Flex p="2" bg="transparent" color="white" boxShadow="md" position="sticky" bottom={0} zIndex={3}>
-      <Text fontSize="sm">© 2023 ASL Journey</Text>
-      <Spacer />
-      <RouterLink href="#" color="white">Privacy Policy</RouterLink>
-      <RouterLink ml="5" href="#" color="white">Terms of Service</RouterLink>
-    </Flex>
-  );
-}
-
-
-function Home() {
+  
+  // Footer
+  function Footer() {
+    return (
+      <Flex p="2" bg="transparent" color="white" boxShadow="md" position="sticky" bottom={0} zIndex={3}>
+        <Text fontSize="sm">© 2023 My Portfolio</Text>
+        <Spacer />
+        <RouterLink href="#" color="white">Privacy Policy</RouterLink>
+        <RouterLink ml="5" href="#" color="white">Terms of Service</RouterLink>
+      </Flex>
+    );
+  }
+  
+  function Home() {
     const bgGradient = useColorModeValue("linear(to-br, #4b5178, #3a4062)", "linear(to-br, #4b5178, #3a4062)");
   
     return (
@@ -101,10 +102,10 @@ function Home() {
           transition={{ duration: 0.5 }}
         >
           <Canvas style={{ position: "absolute", zIndex: 1, background: "#000000" }}>
-          <ambientLight />
-          <pointLight position={[10, 10, 10]} />
-          {[...Array(200)].map((_, i) => <Model key={i} />)}
-        </Canvas>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            {[...Array(200)].map((_, i) => <Model key={i} />)}
+          </Canvas>
   
           <Box 
             zIndex={2} 
@@ -117,33 +118,44 @@ function Home() {
             borderColor={useColorModeValue("#4b5178", "white")}
           >
             <VStack spacing={2} textAlign="center" maxW={'lg'}>
+              {/* Replace with your image */}
+              <Image 
+  src={myImage} 
+  alt="my photo" 
+  borderRadius="full" 
+  boxSize="125px" 
+  objectFit="cover" 
+  borderWidth="2px"
+  borderStyle="solid"
+  borderColor={useColorModeValue("#4b5178", "white")}
+/>
               <Heading 
-                fontSize="6xl" 
+                fontSize="4xl" 
                 fontWeight="bold" 
                 lineHeight="tight" 
                 letterSpacing={"-.1rem"} 
                 color={useColorModeValue("white", "white")}
               >
-                Start Your ASL Journey
+                Your Name
               </Heading>
               <Text 
-                fontSize="2xl" 
+                fontSize="xl" 
                 color={useColorModeValue("white", "white")}
               >
-                Dive into the exciting world of sign language. Ready to get started?
+                Brief about you, your work and skills
               </Text>
-              <RouterLink to="/login">
-      <MotionButton
-        whileHover="hover"
-        whileTap="tap"
-        variants={buttonVariants}
-        colorScheme="purple"
-        size="lg"
-        color="white"
-      >
-        Let's Go!
-      </MotionButton>
-  </RouterLink>
+              <RouterLink to="/about">
+                <MotionButton
+                  whileHover="hover"
+                  whileTap="tap"
+                  variants={buttonVariants}
+                  colorScheme="purple"
+                  size="lg"
+                  color="white"
+                >
+                  Learn More
+                </MotionButton>
+              </RouterLink>
             </VStack>
           </Box>
         </MotionBox>
@@ -153,6 +165,5 @@ function Home() {
     );
   }
   
-  
-  
   export default Home;
+  
