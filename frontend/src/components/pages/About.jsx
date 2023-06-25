9960a2a2937b3c245168eff966c93a8414dfefb4
adriannapinzariu <121136061+adriannapinzariu@users.noreply.github.com>
2023-06-25 18:06:23 -0500

    new bar for features

--- frontend/src/components/pages/About.jsx
+++ frontend/src/components/pages/About.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { Box, VStack, Heading, Text, Progress, UnorderedList, ListItem, useColorModeValue } from '@chakra-ui/react';
+import { Box, VStack, Heading, Text, Progress, UnorderedList, ListItem, useColorModeValue, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
 
 import { motion } from 'framer-motion';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
@@ -62,76 +62,102 @@ function TimelineEvent({ date, event }) {
     );
   }
 
-function About() {
-  const bgGradient = useColorModeValue("linear(to-br, #4b5178, #3a4062)", "linear(to-br, #4b5178, #3a4062)");
-
-  return (
-    <Box minHeight="100vh" display="flex" flexDirection="column" backgroundColor="#000000">
-      <Navbar />
-
-      <MotionBox
-        flex="1"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        bgGradient={bgGradient}
-        padding={4}
-        animate={{ scale: [0.95, 1], opacity: [0, 1] }}
-        transition={{ duration: 0.5 }}
-      >
-        <Canvas style={{ position: "absolute", zIndex: 1, background: "#000000" }}>
-          <ambientLight />
-          <pointLight position={[10, 10, 10]} />
-          {[...Array(200)].map((_, i) => <Model key={i} />)}
-        </Canvas>
-
-        <Box
-          zIndex={2}
-          bg="#1a202c"
-          p={5}
-          rounded="xl"
-          shadow="2xl"
-          backdropFilter="blur(10px)"
-          borderWidth={2}
-          borderColor={useColorModeValue("#4b5178", "white")}
+  function About() {
+    const bgGradient = useColorModeValue("linear(to-br, #4b5178, #3a4062)", "linear(to-br, #4b5178, #3a4062)");
+  
+    return (
+      <Box minHeight="100vh" display="flex" flexDirection="column" backgroundColor="#000000">
+        <Navbar />
+  
+        <MotionBox
+          flex="1"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          bgGradient={bgGradient}
+          padding={4}
+          animate={{ scale: [0.95, 1], opacity: [0, 1] }}
+          transition={{ duration: 0.5 }}
         >
-          <VStack spacing={2} textAlign="center" maxW={'lg'}>
-            <Heading
-              fontSize="4xl"
-              fontWeight="bold"
-              lineHeight="tight"
-              letterSpacing={"-.1rem"}
-              color={useColorModeValue("white", "white")}
-            >
-              About Me
-            </Heading>
-            <Text
-              fontSize="xl"
-              color={useColorModeValue("white", "white")}
-            >
-              From building and racing electric vehicles to researching how AI can combat human trafficking, I am always seeking out new and innovative ways to make a meaningful difference. I have also had the honor of delivering award-winning speeches on the important topics of women in STEM and algorithmic bias. My track record of success speaks for itself, having held 8 leadership positions within the past year and continuously inspiring and motivating others towards a common goal.
-            </Text>
-          <Heading fontSize="2xl" color={useColorModeValue("white", "white")}>Work Experience & Education</Heading>
-          <UnorderedList>
-            <TimelineEvent date="2018-2022" event="Software Developer at XYZ" />
-            <TimelineEvent date="2014-2018" event="BSc in Computer Science at ABC University" />
-            {/* Add more TimelineEvent components here */}
-          </UnorderedList>
-          <Heading fontSize="2xl" color={useColorModeValue("white", "white")}>Skills</Heading>
-          <SkillBar skill="JavaScript" proficiency={90} />
-          <SkillBar skill="React" proficiency={85} />
-          {/* Add more SkillBar components here */}
-          <Heading fontSize="2xl" color={useColorModeValue("white", "white")}>Fun Facts</Heading>
-          <Text color={useColorModeValue("white", "white")}>I'm a chess enthusiast and have a 3D printer at home!</Text>
-          {/* Add more fun facts */}
-        </VStack>
-        </Box>
-      </MotionBox>
-
-      <Footer />
-    </Box>
-  );
-}
+          <Canvas style={{ position: "absolute", zIndex: 1, background: "#000000" }}>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            {[...Array(200)].map((_, i) => <Model key={i} />)}
+          </Canvas>
+  
+          <Box
+            zIndex={2}
+            bg="#1a202c"
+            p={5}
+            rounded="xl"
+            shadow="2xl"
+            backdropFilter="blur(10px)"
+            borderWidth={2}
+            borderColor={useColorModeValue("#4b5178", "white")}
+          >
+            <Tabs colorScheme="purple" variant="soft-rounded" size="lg" isFitted>
+              <TabList>
+                <Tab>About</Tab>
+                <Tab>Experience</Tab>
+                <Tab>Education</Tab>
+                <Tab>Hobbies</Tab>
+              </TabList>
+  
+              <TabPanels>
+                <TabPanel>
+                  <VStack spacing={2} textAlign="center" maxW={'lg'}>
+                    <Heading
+                      fontSize="4xl"
+                      fontWeight="bold"
+                      lineHeight="tight"
+                      letterSpacing={"-.1rem"}
+                      color={useColorModeValue("white", "white")}
+                    >
+                      About Me
+                    </Heading>
+                    <Text
+                      fontSize="xl"
+                      color={useColorModeValue("white", "white")}
+                    >
+                      From building and racing electric vehicles to researching how AI can combat human trafficking, I am always seeking out new and innovative ways to make a meaningful difference. I have also had the honor of delivering award-winning speeches on the important topics of women in STEM and algorithmic bias. My track record of success speaks for itself, having held 8 leadership positions within the past year and continuously inspiring and motivating others towards a common goal.
+                    </Text>
+                  </VStack>
+                </TabPanel>
+                <TabPanel>
+                  <VStack spacing={2} textAlign="center" maxW={'lg'}>
+                    <Heading fontSize="2xl" color={useColorModeValue("white", "white")}>Work Experience</Heading>
+                    <UnorderedList>
+                      <TimelineEvent date="2018-2022" event="Software Developer at XYZ" />
+                      {/* Add more TimelineEvent components here */}
+                    </UnorderedList>
+                  </VStack>
+                </TabPanel>
+                <TabPanel>
+                  <VStack spacing={2} textAlign="center" maxW={'lg'}>
+                    <Heading fontSize="2xl" color={useColorModeValue("white", "white")}>Education</Heading>
+                    <UnorderedList>
+                      <TimelineEvent date="2014-2018" event="BSc in Computer Science at ABC University" />
+                      {/* Add more TimelineEvent components here */}
+                    </UnorderedList>
+                  </VStack>
+                </TabPanel>
+                <TabPanel>
+                  <VStack spacing={2} textAlign="center" maxW={'lg'}>
+                    <Heading fontSize="2xl" color={useColorModeValue("white", "white")}>Hobbies</Heading>
+                    <Text color={useColorModeValue("white", "white")}>I'm a chess enthusiast and have a 3D printer at home!</Text>
+                    {/* Add more hobbies here */}
+                  </VStack>
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
+          </Box>
+        </MotionBox>
+  
+        <Footer />
+      </Box>
+    );
+  }
+  
 
 export default About;
 
