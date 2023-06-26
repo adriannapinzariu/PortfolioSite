e742b10e4631993aee8a70aa4f0dea5314590ea7
lilllmeng <lilllmeng@genius.com>
2023-06-26 00:48:59 -0700

    hobby data from backend

--- frontend/src/components/pages/Hobby.jsx
+++ frontend/src/components/pages/Hobby.jsx
@@ -5,109 +5,114 @@ import { Box, VStack, Heading, Text, Progress, UnorderedList, ListItem, useColor
 import { motion } from 'framer-motion';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { MeshBasicMaterial } from 'three';
+
 import Navbar from '../Navbar';
 import Footer from '../Footer';
-import hobby1 from "../../Assets/hobby1.jpg";
-import hobby2 from "../../Assets/hobby2.jpg";
-import hobby3 from "../../Assets/hobby3.jpg";
+
 const MotionBox = motion(Box);
 
 function Model() {
-    const ref = useRef();
-    const colors = ['#ADD8E6', '#9DB2E1', '#8D8CDC', '#7D66D7', '#6D40D2', '#5C1ACD', '#4B00C9'];
-    const color = colors[Math.floor(Math.random() * colors.length)];
-    const [model, setModel] = useState();
+  const ref = useRef();
+  const colors = ['#ADD8E6', '#9DB2E1', '#8D8CDC', '#7D66D7', '#6D40D2', '#5C1ACD', '#4B00C9'];
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  const [model, setModel] = useState();
 
-    useEffect(() => {
-        new GLTFLoader().load('/3D/puzzlePiece1.glb', (gltf) => {
-            gltf.scene.traverse((child) => {
-                if (child.isMesh) {
-                    child.material = new MeshBasicMaterial({ wireframe: true, color });
-                }
-            });
-            setModel(gltf);
-        });
-    }, [])
-
-    useFrame(() => {
-        if (ref.current) {
-            ref.current.position.y -= 0.1;
-            if (ref.current.position.y < -50) ref.current.position.y = 100;
-            ref.current.rotation.x += 0.01;
-            ref.current.rotation.y += 0.01;
+  useEffect(() => {
+    new GLTFLoader().load('/3D/puzzlePiece1.glb', (gltf) => {
+      gltf.scene.traverse((child) => {
+        if (child.isMesh) {
+          child.material = new MeshBasicMaterial({ wireframe: true, color });
         }
+      });
+      setModel(gltf);
     });
+  }, []);
 
-    return model ? <primitive object={model.scene} position={[Math.random() * 100 - 50, Math.random() * 100, Math.random() * 100 - 50]} ref={ref} /> : null;
+  useFrame(() => {
+    if (ref.current) {
+      ref.current.position.y -= 0.1;
+      if (ref.current.position.y < -50) ref.current.position.y = 100;
+      ref.current.rotation.x += 0.01;
+      ref.current.rotation.y += 0.01;
+    }
+  });
+
+  return model ? <primitive object={model.scene} position={[Math.random() * 100 - 50, Math.random() * 100, Math.random() * 100 - 50]} ref={ref} /> : null;
 }
-
 
 function Hobby() {
-    const bgGradient = useColorModeValue("linear(to-br, #4b5178, #3a4062)", "linear(to-br, #4b5178, #3a4062)");
+  const bgGradient = useColorModeValue("linear(to-br, #4b5178, #3a4062)", "linear(to-br, #4b5178, #3a4062)");
+  const url = `http://127.0.0.1:5000/hobby`;
+  const [data, setData] = useState([]);
 
-    return (
-        <Box minHeight="100vh" display="flex" flexDirection="column" backgroundColor="#000000">
-            <Navbar />
+  useEffect(() => {
+    getHobby(url);
+  }, []);
 
-            <MotionBox
-                flex="1"
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-                bgGradient={bgGradient}
-                padding={4}
-                animate={{ scale: [0.95, 1], opacity: [0, 1] }}
-                transition={{ duration: 0.5 }}
-            >
-                <Canvas style={{ position: "absolute", zIndex: 1, background: "#000000" }}>
-                    <ambientLight />
-                    <pointLight position={[10, 10, 10]} />
-                    {[...Array(200)].map((_, i) => <Model key={i} />)}
-                </Canvas>
+  const colorModeValue = useColorModeValue("#4b5178", "white");
 
-                <Box
-                    zIndex={2}
-                    bg="#1a202c"
-                    p={5}
-                    rounded="xl"
-                    shadow="2xl"
-                    backdropFilter="blur(10px)"
-                    borderWidth={2}
-                    borderColor={useColorModeValue("#4b5178", "white")}
-                >
-                    <Tabs colorScheme="purple" variant="soft-rounded" size="lg" isFitted>
+  function getHobby(url) {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setData(data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
 
-                        <TabPanels>
-                            <TabPanel>
-                                <VStack spacing={6} alignItems="flex-start" maxW={'lg'}>
-                                    <Heading fontSize="2xl" color={useColorModeValue("white", "white")}>Hobbies</Heading>
-                                    <Box w="full" p={4} borderBottom="1px" borderBottomColor="whiteAlpha.600">
-                                        <Text mb="1" fontSize="xl" color={useColorModeValue("white", "white")}>Hobby 1</Text>
-                                        <img src={hobby1}/>
-                                    </Box>
-                                    <Box w="full" p={4} borderBottom="1px" borderBottomColor="whiteAlpha.600">
-                                        <Text mb="1" fontSize="xl" color={useColorModeValue("white", "white")}>Hobby 2</Text>
-                                        <img src={hobby2}/>
+  return (
+    <Box minHeight="100vh" display="flex" flexDirection="column" backgroundColor="#000000">
+      <Navbar />
 
-                                    </Box>
-                                    <Box w="full" p={4} borderBottom="1px" borderBottomColor="whiteAlpha.600">
-                                        <Text mb="1" fontSize="xl" color={useColorModeValue("white", "white")}>Hobby 3</Text>
-                                        <img src={hobby3}/>
-                                    </Box>
+      <MotionBox
+        flex="1"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        bgGradient={bgGradient}
+        padding={4}
+        animate={{ scale: [0.95, 1], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+      >
+        <Canvas style={{ position: "absolute", zIndex: 1, background: "#000000" }}>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          {[...Array(200)].map((_, i) => <Model key={i} />)}
+        </Canvas>
 
-                                </VStack>
-                            </TabPanel>
-
-
-                        </TabPanels>
-                    </Tabs>
-                </Box>
-            </MotionBox>
-
-            <Footer />
+        <Box
+          zIndex={2}
+          bg="#1a202c"
+          p={5}
+          rounded="xl"
+          shadow="2xl"
+          backdropFilter="blur(10px)"
+          borderWidth={2}
+          borderColor={colorModeValue}
+        >
+          <Tabs colorScheme="purple" variant="soft-rounded" size="lg" isFitted>
+            <TabPanels>
+              <TabPanel>
+                <VStack spacing={6} alignItems="flex-start" maxW={'lg'}>
+                  <Heading fontSize="2xl" color="white">Hobbies</Heading>
+                  {data.map((item) => (
+                    <Box w="full" p={4}>
+                      <Text mb="1" fontSize="xl" color="white">{item.name}</Text>
+                      <img src={item.img} alt={item.name} />
+                    </Box>
+                  ))}
+                </VStack>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
         </Box>
-    );
-}
+      </MotionBox>
 
+      <Footer />
+    </Box>
+  );
+}
 
 export default Hobby;
