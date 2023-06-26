beb0628b418757e947deb262a884cdb7c98d69cb
lilllmeng <lilllmeng@genius.com>
2023-06-26 01:47:53 -0700

    location data

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/home')
 def home():
     return jsonify({"message": "Connected successfully"})
+
 
 @app.route('/')
 def index():
@@ -15,10 +17,11 @@ def index():
     }
     return jsonify(response_body)
 
+
 @app.route('/hobby')
 def get_hobby():
     response_body = [
-   {
+        {
             "name": "Game",
             "img": "https://images.pexels.com/photos/1174746/pexels-photo-1174746.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         },
@@ -35,6 +38,7 @@ def get_hobby():
     ]
     return jsonify(response_body)
 
+
 @app.route('/experience')
 def get_experience():
     response_body = [
@@ -48,5 +52,22 @@ def get_experience():
             "time": "2020-2023",
             "responsibilities": "Worked as part of a team to maintain and enhance the company website. Implemented agile development methodologies to improve productivity"
         }
+    ]
+    return jsonify(response_body)
+
+
+@app.route('/location')
+def get_location():
+    response_body = [
+        {"country": "Canada", "lat": 45.421532, "long": -75.697189},
+        {"country": "Canada", "lat": 45.421532, "long": -75.697189},
+        {"country": "Mexico", "lat": 19.432608, "long": -99.133209},
+        {"country": "US", "lat": 38.9071923, "long": -77.0368707},
+        {"country": "UK", "lat": 51.509865, "long": -0.118092},
+        {"country": "France", "lat": 48.864716, "long": 2.349014},
+        {"country": "Spain", "lat": 40.416775, "long": -3.703790},
+        {"country": "Japan", "lat": 36.2048, "long": 138.2529},
+        {"country": "Tunisia", "lat": 33.8869, "long": 9.5375},
+        {"country": "South Korea", "lat": 35.9078, "long": 127.7669},
     ]
     return jsonify(response_body)
