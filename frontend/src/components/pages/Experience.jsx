de8da1b0ba0649083d129c862cbd9253e0f4a293
lilllmeng <lilllmeng@genius.com>
2023-06-26 10:14:27 -0700

    Fetch experience data from backend

--- frontend/src/components/pages/Experience.jsx
+++ frontend/src/components/pages/Experience.jsx
@@ -39,14 +39,45 @@ function Model() {
     return model ? <primitive object={model.scene} position={[Math.random() * 100 - 50, Math.random() * 100, Math.random() * 100 - 50]} ref={ref} /> : null;
 }
 
-
 function Experience() {
     const bgGradient = useColorModeValue("linear(to-br, #4b5178, #3a4062)", "linear(to-br, #4b5178, #3a4062)");
+    const workUrl = `http://127.0.0.1:5000/experience`;
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        getExperience(workUrl);
+    }, []);
 
+    function getExperience(workUrl) {
+        fetch(workUrl)
+            .then(response => response.json())
+            .then(data => {
+                setData(data);
+                console.log(data)
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
+    const eduUrl = `http://127.0.0.1:5000/education`;
+    const [data2, setData2] = useState([]);
+    useEffect(() => {
+        getEdducation(eduUrl);
+    }, []);
+
+    function getEdducation(eduUrl) {
+        fetch(eduUrl)
+            .then(response => response.json())
+            .then(data2 => {
+                setData2(data2);
+                console.log(data2)
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
     return (
         <Box minHeight="100vh" display="flex" flexDirection="column" backgroundColor="#000000">
             <Navbar />
-
             <MotionBox
                 flex="1"
                 display="flex"
@@ -62,7 +93,6 @@ function Experience() {
                     <pointLight position={[10, 10, 10]} />
                     {[...Array(200)].map((_, i) => <Model key={i} />)}
                 </Canvas>
-
                 <Box
                     zIndex={2}
                     bg="#1a202c"
@@ -74,66 +104,58 @@ function Experience() {
                     borderColor={useColorModeValue("#4b5178", "white")}
                 >
                     <Tabs colorScheme="purple" variant="soft-rounded" size="lg" isFitted>
+                        <TabList>
+                            <Tab>Work Experience</Tab>
+                            <Tab>Education</Tab>
+                        </TabList>
 
                         <TabPanels>
                             <TabPanel>
                                 <VStack spacing={6} alignItems="flex-start" maxW={'lg'}>
-                                    <Heading fontSize="2xl" color={useColorModeValue("white", "white")}>Work Experience</Heading>
-                                    <Box w="full" p={4} borderBottom="1px" borderBottomColor="whiteAlpha.600">
-                                        <Text mb="1" fontSize="xl" color={useColorModeValue("white", "white")}>Senior Software Developer at XYZ Corp</Text>
-                                        <Text mb="1" color={useColorModeValue("white", "white")}>2023-Present</Text>
-                                        <Heading mb="1" fontSize="md" color={useColorModeValue("white", "white")}>Responsibilities:</Heading>
-                                        <UnorderedList color={useColorModeValue("white", "white")}>
-                                            <ListItem mb="1">Lead a team of developers to maintain and enhance the company website</ListItem>
-                                            <ListItem mb="1">Implemented agile development methodologies to improve productivity</ListItem>
-                                        </UnorderedList>
-                                        <Heading mb="1" fontSize="md" color={useColorModeValue("white", "white")}>Achievements:</Heading>
-                                        <UnorderedList color={useColorModeValue("white", "white")}>
-                                            <ListItem mb="1">Redesigned the company website, increasing traffic by 30%</ListItem>
-                                            <ListItem mb="1">Developed an automated testing suite, reducing bugs by 15%</ListItem>
-                                        </UnorderedList>
-                                    </Box>
-
-                                    <Box w="full" p={4} borderBottom="1px" borderBottomColor="whiteAlpha.600">
-                                        <Text mb="1" fontSize="xl" color={useColorModeValue("white", "white")}>Software Developer at ABC Inc</Text>
-                                        <Text mb="1" color={useColorModeValue("white", "white")}>2020-2023</Text>
-                                        <Heading mb="1" fontSize="md" color={useColorModeValue("white", "white")}>Responsibilities:</Heading>
-                                        <UnorderedList color={useColorModeValue("white", "white")}>
-                                            <ListItem mb="1">Developed new features for the company's main product</ListItem>
-                                            <ListItem mb="1">Maintained the codebase, debugging and fixing issues</ListItem>
-                                        </UnorderedList>
-                                        <Heading mb="1" fontSize="md" color={useColorModeValue("white", "white")}>Achievements:</Heading>
-                                        <UnorderedList color={useColorModeValue("white", "white")}>
-                                            <ListItem mb="1">Improved product performance by 20% with optimization techniques</ListItem>
-                                            <ListItem mb="1">Received Employee of the Year award in 2022</ListItem>
-                                        </UnorderedList>
-                                    </Box>
-
-                                    <Box w="full" p={4}>
-                                        <Text mb="1" fontSize="xl" color={useColorModeValue("white", "white")}>Junior Software Developer at DEF Ltd</Text>
-                                        <Text mb="1" color={useColorModeValue("white", "white")}>2018-2020</Text>
-                                        <Heading mb="1" fontSize="md" color={useColorModeValue("white", "white")}>Responsibilities:</Heading>
-                                        <UnorderedList color={useColorModeValue("white", "white")}>
-                                            <ListItem mb="1">Worked in a team to develop and maintain several web applications</ListItem>
-                                            <ListItem mb="1">Contributed to code reviews and software design decisions</ListItem>
-                                        </UnorderedList>
-                                        <Heading mb="1" fontSize="md" color={useColorModeValue("white", "white")}>Achievements:</Heading>
-                                        <UnorderedList color={useColorModeValue("white", "white")}>
-                                            <ListItem mb="1">Contributed to a project that won the Best Software Design award</ListItem>
-                                            <ListItem mb="1">Improved application security, reducing security issues by 25%</ListItem>
-                                        </UnorderedList>
-                                    </Box>
+                                    <Heading fontSize="2xl" color="white">Work Experience</Heading>
+                                    {data.map((item, index) => (
+                                        <Box key={index} w="full" p={4} borderBottom="1px" borderBottomColor="whiteAlpha.600">
+                                            <Text mb="1" fontSize="xl" color="white">{item.title}</Text>
+                                            <Text mb="1" color="white">{item.time}</Text>
+                                            <Heading mb="1" fontSize="md" color="white">Responsibilities:</Heading>
+                                            <UnorderedList color="white">
+                                                {Array.isArray(item.responsibility) && item.responsibility.map((responsibility, resIndex) => (
+                                                    <ListItem key={resIndex}>{responsibility}</ListItem>
+                                                ))}
+                                            </UnorderedList>
+                                            <Heading mb="1" fontSize="md" color="white">Achievements:</Heading>
+                                            <UnorderedList color="white">
+                                                {Array.isArray(item.achievement) && item.achievement.map((achievement, achIndex) => (
+                                                    <ListItem key={achIndex}>{achievement}</ListItem>
+                                                ))}
+                                            </UnorderedList>
+                                        </Box>
+                                    ))}
                                 </VStack>
                             </TabPanel>
-                        
 
+                            <TabPanel>
+                                <VStack spacing={6} alignItems="flex-start" maxW={'lg'}>
+                                    <Heading fontSize="2xl" color="white">Education</Heading>
+                                    {data2.map((item, index) => (
+                                        <Box key={index} w="full" p={4} borderBottom="1px" borderBottomColor="whiteAlpha.600">
+                                            <Text mb="1" fontSize="xl" color="white">{item.title}</Text>
+                                            <Text mb="1" color="white">{item.time}</Text>
+                                            <Heading mb="1" fontSize="md" color="white">Courseworks:</Heading>
+                                            <UnorderedList color="white">
+                                                {Array.isArray(item.coursework) && item.coursework.map((coursework, courIndex) => (
+                                                    <ListItem key={courIndex}>{coursework}</ListItem>
+                                                ))}
+                                            </UnorderedList>
+                                        </Box>
+                                    ))}
+                                </VStack>
+                            </TabPanel>
                         </TabPanels>
-                        
-                        
                     </Tabs>
+
                 </Box>
             </MotionBox>
-
             <Footer />
         </Box>
     );
