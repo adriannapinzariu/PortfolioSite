e803928e64fae4c8d51d7fa3fdaeaf324e19bd74
adriannapinzariu <121136061+adriannapinzariu@users.noreply.github.com>
2023-07-14 17:15:57 -0500

    ORM mode

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,18 @@ mydb = MySQLDatabase(
     host=DATABASE_HOST
 )
 
-print(mydb)
+#print(mydb)
+
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = mydb  
+mydb.connect()
+mydb.create_tables([TimelinePost])
 
 
 
