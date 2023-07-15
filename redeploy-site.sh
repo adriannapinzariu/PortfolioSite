f32388081273eee9775be6d36ba1f65e2b3d3504
adriannapinzariu <121136061+adriannapinzariu@users.noreply.github.com>
2023-07-15 15:10:45 -0500

    Updated redeploy-site.sh script to use systemd service

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
+set -e
 
-tmux kill-server
-cd /root/PortfolioSite/app
+cd /root/PortfolioSite
 git fetch && git reset --hard origin/main
-python3 -m venv venv
-source venv/bin/activate
-pip install --upgrade pip
-pip install setuptools_rust
-pip install -r ../requirements.txt
-sudo dnf install python3-pip
-tmux new-session -d -s flask_session
-tmux send-keys -t flask_session 'source venv/bin/activate' C-m
-tmux send-keys -t flask_session 'export FLASK_APP=__init__.py' C-m
-tmux send-keys -t flask_session 'flask run --host=0.0.0.0' C-m
 
+source python3-virtualenv/bin/activate
+pip install -r requirements.txt
+deactivate
 
+sudo systemctl restart myportfolio
 
