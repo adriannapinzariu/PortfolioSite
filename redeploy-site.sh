f335d9c02ff9874400b4140ad32bf13c7ed0b984
adriannapinzariu <adriannapinzariu@gmail.com>
2023-08-26 21:26:12 +0000

    Site Deployment Test

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -4,9 +4,9 @@ set -e
 cd /root/PortfolioSite
 git fetch && git reset --hard origin/main
 
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-deactivate
+#source python3-virtualenv/bin/activate
+#pip install -r requirements.txt
+#deactivate
 
 
 sudo systemctl restart myportfolio
