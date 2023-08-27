0cdea2951755de4415af210ce8797f24012bae5f
adriannapinzariu <adriannapinzariu@gmail.com>
2023-08-27 04:25:02 +0000

    i think im getting closer

--- app/__init__.py
+++ app/__init__.py
@@ -36,7 +36,7 @@ if os.getenv("TESTING") == "true":
     print("Running in test mode")
     mydb = SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
 else:
-    mydb = MySQLDatabase(os.getenv("MySQLDatabase"),
+    mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
     host = os.getenv("MYSQL_HOST"),
     user = os.getenv("MYSQL_USER"),
     password = os.getenv("MYSQL_PASSWORD"),
