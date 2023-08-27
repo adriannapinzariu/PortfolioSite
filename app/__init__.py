1de9a0a430a819f116702e0309e63f30801bf256
adriannapinzariu <adriannapinzariu@gmail.com>
2023-08-27 03:31:42 +0000

    test

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 CORS(app)
 
 from dotenv import load_dotenv
-env_path = os.path.join(os.path.dirname(__file__), '..', 'example.env')
+env_path = os.path.join(os.path.dirname(__file__), '..', '.env')
 load_dotenv(dotenv_path=env_path)
 
 
