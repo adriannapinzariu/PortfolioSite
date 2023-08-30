480e1c27ffde6f9c89cb96b26615c3a1ca3a24e7
adriannapinzariu <adriannapinzariu@gmail.com>
2023-08-30 02:09:18 +0000

    Fixed Run Tests Shell Script

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/Users/adriannapinzariu/Site/PortfolioSite/app/python3-virtualenv/bin/python3 -m unittest discover -v tests/
+../app/python3-virtualenv/bin/python3 -m unittest discover -v tests/
